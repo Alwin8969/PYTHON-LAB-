24e43056137932d4e64a00d116d142236e9e33c6
Alwin8969 <alwinjoshy2028@cs.sjcetpalai.ac.in>
2024-10-05 16:24:48 +0530

    Add files via upload

---
+++ task 4 maths.py
@@ -0,0 +1,8 @@
+import math
+num=int(input("Enter a number:"))
+sq_root=math.sqrt(num)
+factorial=math.factorial(num)
+raised_pow=math.pow(num,2)
+print("Square root of 5:",sq_root)
+print("Factorial OF 5:",factorial)
+print("5 raised to power 2:",raised_pow)
---
+++ twinkle.py
@@ -0,0 +1,4 @@
+#author: Alwin Joshy
+#Date:05/10/2024
+string="Twinkle, twinkle, little star, \n\t How I wonder what you are! \n\t\t Up above there world so high, \n\t\t Like a diamond in the sky.\n Twinkle, twinkle, littile star,\n\t How I wonder what you are"
+print(string)
