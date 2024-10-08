b96f14581abc19aae2aed9ebda81c362aa894507
Alwin8969 <alwinjoshy2028@cs.sjcetpalai.ac.in>
2024-10-08 14:59:56 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+first_name="Alwin"
+last_name="Joshy"
+full_name=first_name+' '+ last_name
+length=len(first_name)
+extracted_last_name=full_name[length+1:]
+print(full_name)
+print(length)
+print(extracted_last_name)
