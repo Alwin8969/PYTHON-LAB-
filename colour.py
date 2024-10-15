93c42dd85593db865bcab460177097e3f4fb4066
Alwin8969 <alwinjoshy2028@cs.sjcetpalai.ac.in>
2024-10-15 15:38:25 +0530

    Add files via upload

---
+++ colour.py
@@ -0,0 +1,9 @@
+grade=input("Enter the colour code")
+if grade=="r" or grade=="R":
+    print("Red")
+elif grade=="g" or grade=="G":
+    print("Green")
+elif grade=="b" or grade=="B":
+    print("Blue")
+else:
+    print("Wrong code")
